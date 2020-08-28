9e716ff4af80164ef9b2a17e435ee733c943eb81
Owangatang <shaunwangg@gmail.com>
2020-08-28 11:19:51 -0400

    logic update

--- logic.js
+++ logic.js
@@ -3,31 +3,15 @@ const menu = {
     appetizers: [],
     mains: [],
     desserts: [],
-    addDishToCourse (courseName, dishName, dishPrice) {
-      const dish = {name:dishName, price:dishPrice};
-      this._courses[courseName].push[dish];
-    },
-    getRandomDishFromCourse(courseName) {
-      let dishes = this._courses[courseName];
-      const randomIndex = Math.floor(Math.random() * dishes.length());
-      return dishes[randomIndex];
-    },
-    generateRandomMeal(){
-      let appetizer = this.getRandomDishFromCourse(appetizers);
-      let main = this.getRandomDishFromCrouse(mains);
-      let dessert = this.getRandomDishFromCourse(desserts);
-      let totalPrice = appetizer.price + main.price + dessert.price;
-      return `The price of your meal is ${totalPrice}`;
-    }
   },
   set appetizers(appetizersIn) {
-    this._courses.appetizers.push(appetizersIn);
+    this._courses.appetizers = appetizersIn;
   },
   set mains(mainsIn) {
-    this._courses.mains.push(mainsIn);
+    this._courses.mains = mainsIn;
   },
   set desserts(dessertsIn) {
-    this._courses.desserts.push(dessertsIn);
+    this._courses.desserts = dessertsIn;
   },
   get appetizers() {
     return this._courses.appetizers;
@@ -37,10 +21,46 @@ const menu = {
   },
   get dessert() {
     return this._courses.dessert;
+  },
+  get courses() {
+    return {
+      appetizers: this.appetizers,
+      mains: this.mains,
+      desserts: this.desserts,
+    }
+  },
+  addDishToCourse (courseName, dishName, dishPrice) {
+      const dish = {
+      name: dishName, 
+      price: dishPrice,
+      };
+      this._courses[courseName].push(dish);
+  },
+  getRandomDishFromCourse(courseName) {
+      const dishes = this._courses[courseName];
+      const randomIndex = Math.floor(Math.random() * dishes.length);
+      return dishes[randomIndex];
+  },
+  generateRandomMeal(){
+      const appetizer = this.getRandomDishFromCourse('appetizers');
+      const main = this.getRandomDishFromCourse('mains');
+      const dessert = this.getRandomDishFromCourse('desserts');
+      const totalPrice = appetizer.price + main.price + dessert.price;
+      return `The meal for today starts with ${appetizer.name}. Following, we will serve a delicious ${main.name}. At the end, we will finish with a ${dessert.name}. The total price of your meal comes out to $${totalPrice}`;
   }
 };
 
 menu.addDishToCourse('appetizers', 'Tuna TarTar', 5);
-menu.addDishToCourse('appetizers', 'Steak TarTar', 6);
+menu.addDishToCourse('appetizers', 'Chopped Salad', 4);
 menu.addDishToCourse('appetizers', 'Shark Ceviche', 5);
-menu.addDishToCourse('appetizers', 'Tuna TarTar', 5);
+
+menu.addDishToCourse('mains', 'Lamb Shank', 5);
+menu.addDishToCourse('mains', 'Salmon Pizza', 10);
+menu.addDishToCourse('mains', 'Pasta', 8);
+
+menu.addDishToCourse('desserts', 'Chocolate Cake', 5);
+menu.addDishToCourse('desserts', 'Espresso', 6);
+menu.addDishToCourse('desserts', 'Ice Cream', 4);
+
+const meal = menu.generateRandomMeal()
+console.log(meal);
